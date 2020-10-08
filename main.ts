a947c91e4d54eb1db1f49c74876a7e3a5a8e4851
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-10-08 12:41:23 -0400

    First commit

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+// Created by Vaughn Hatherall on October 8 to calculate cost of pizzas
+//  
+game.splash("Hi Welcome To Papa Johns press A so I can take your order")
+let Width = game.askForNumber("Ok and how wide do you want that pizza (inches)")
+let Subtotal = 0.5 * (Width + (0.75 + 1))
+let total = 1.13 * Subtotal
+total = Math.round(0)
